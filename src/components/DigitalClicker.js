a1d1ef08b51fb099c2dffd51e4753e082ceccee8
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-02-20 23:09:24 -0800

    commit tree bcc469132570ae78ecf2797d63abe7d128cdb8d0

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {    
+
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0,
+        };  
+    }
+
+    timesClicked = () => {
+        let newCount = this.state.timesClicked + 1;
+        this.setState({
+          timesClicked: newCount,
+        });
+      };
+
+    render() {    
+        return (    
+            <div>
+                <button onClick={this.timesClicked}>{this.state.timesClicked}</button>
+            </div>    
+        )    
+    }
+}
+
